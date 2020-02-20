2d41c5b2a3ee6816d5037be72f9a904a37f2f26c
jacheg <jacchegia@gmail.com>
2020-02-20 02:26:49 +0000

    Did UI, figure out API please

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-/* global $ */
+$(".btn").click(function(){
+  let term = $(".form-control").val();
+  console.log(term);
+  fetch(`https://api.giphy.com/v1/gifs/search?q=${term}&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`)
+  .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          $(".gif").append(`<img src="${data[0].images.original.url}">`);
+          console.log(data);
+        });
 
-console.log('hi');
 
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
 });
